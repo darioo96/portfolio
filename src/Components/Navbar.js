1309e2dddc1cb198ce2f0cac437f7c315674ff4b
Dario Ortega <dariolean10@gmail.com>
2022-04-25 19:31:39 -0300

    media menu

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,17 +3,25 @@ import { Link } from 'react-router-dom'
 import styles from "./Styles/Navbar.module.css"
 
 export default function Navbar() {
+  const scroll = (num) =>{
+    console.log("clickeado");
+  }
+
   return (
     <div className={styles.container}>
         <div className={styles.ban}></div>
         <div className={styles.logo}>
             <Link to={"/"}><h1>{"<DO/>"}</h1></Link>
         </div>
+        <span className="material-symbols-outlined">
+          menu
+        </span>
         <div className={styles.links}>
+        
           <ul>
-            <li><Link to={"/aboutme"}>About me</Link></li>
-            <li><Link to={"/proyectos"}>Proyectos</Link></li>
-            <li><Link to={"/herramientas"}>Herramientas</Link></li>
+            <li><p onClick={scroll("200vh")}>About me</p></li>
+            <li><p onClick={scroll("200vh")}>Proyectos</p></li>
+            <li><p onClick={scroll("200vh")}>Herramientas</p></li>
           </ul>
         </div>
     </div>
